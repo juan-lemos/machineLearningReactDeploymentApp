3db03f608430c89542ee9ecbf0106b4cd8ed72af
juanpale <juanpale@users.noreply.github.com>
2017-11-26 17:32:00 -0300

    api call and response on modal

---
+++ app/components/ModalMessageFromApi/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import Edible from '../../images/edible.jpg';
+import Poisonous from '../../images/poisonous.png';
+
+
+function ModalMessageFromApi({ edible, percentage, onClose }) {
+  let precision = percentage;
+  if (edible === 'e') {
+    precision = 1 - percentage;
+  }
+
+  return (
+    <Modal.Dialog>
+      <Modal.Body>
+        <div>
+          {edible === 'e' ?
+            <div >
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <img alt={'edible'} src={Edible} />
+              </div>
+              <h3 style={{ color: 'green', display: 'flex', justifyContent: 'center' }}> Edible !</h3>
+            </div>
+            :
+            <div>
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <img alt={'Poisonous'} src={Poisonous} />
+              </div>
+              <h3 style={{ color: 'red', display: 'flex', justifyContent: 'center' }}> Poisonous !</h3>
+            </div>
+          }
+          <h4>{`Precision: ${precision}`}</h4>
+        </div>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button onClick={() => onClose()}>Close</Button>
+      </Modal.Footer>
+    </Modal.Dialog >
+  );
+}
+
+ModalMessageFromApi.propTypes = {
+  edible: PropTypes.string,
+  percentage: PropTypes.number,
+  onClose: PropTypes.func,
+};
+
+export default ModalMessageFromApi;
---
+++ app/components/ModalMessageFromApi/tests/index.test.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+
+// import ModalMessageFromApi from '../index';
+
+describe('<ModalMessageFromApi />', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
